a12676cfd8d91665a7efec72a041af23c4e0adc0
James Butcher <jamesbutcher167@gmail.com>
2022-01-26 20:13:52 +0000

    Add folder_exists help funtion
    This function checks if a folder exists within a given path

--- src/mkdir/mkdir.v
+++ src/mkdir/mkdir.v
@@ -8,5 +8,22 @@ const (
 )
 
 fn main() {
+	println(folder_exists('./test/test2/test3'))
 	os.mkdir('./test') or { eprintln('Directory ‘test’ already exists.') }
+}
+
+fn folder_exists(folder string) bool {
+	mut path := folder.split('/')
+	folder_name := path[path.len - 1]
+
+	path = path[0..path.len - 1] // Omit folder from path
+
+	path_string := path.join('/')
+
+	contents := os.ls(path_string) or {
+		eprintln('Directory ‘$path_string does not exist.’')
+		return true
+	}
+
+	return folder_name in contents
 }
